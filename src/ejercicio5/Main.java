ac71823c1f0c4c6320f5f6681cba9fde2c955879
joantriana7-afk <joantriana7@gmail.com>
2026-04-22 11:53:52 -0500

    ejercicio 5

---
+++ src/ejercicio5/Main.java
@@ -0,0 +1,19 @@
+package ejercicio5;
+/*Al inicio del turno, el sistema pide al cajero que ingrese el monto de apertura de caja.
+El monto debe ser mayor a $0. Si el cajero ingresa un valor inválido, el sistema debe volver a
+pedirlo.
+Simula este comportamiento con un do-while. Como no usamos Scanner en este ejercicio,
+define el valor dentro del código y fuerza al menos una iteración con un valor inválido primero.
+Pregunta: ¿Por qué do-while es más apropiado que while aquí? Describe con tus palabras la diferencia en
+el orden de ejecución*/
+public class Main {
+    public static void main(String[] args) {
+        double montoApertura = 0;
+        do {
+            System.out.println("Monto inválido. Ingrese un monto de apertura.");
+            montoApertura = 50000;
+        } while (montoApertura <= 0);
+
+        System.out.println("Monto de apertura aceptado: " + montoApertura);
+    }
+}
